8dc04f852a46b466af24e776c1dfefb656fb9a18
sohamkasar19 <sohamkasar19@gmail.com>
2023-10-29 22:07:05 -0700

    updated seeder

--- backend/sequelize/seeders/20231027182316-demo-user.js
+++ backend/sequelize/seeders/20231027182316-demo-user.js
@@ -1,10 +1,12 @@
 "use strict";
+const { v4: uuidv4 } = require("uuid");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("users", [
       {
+        id: uuidv4(),
         username: "demoUser",
         email: "demo@user.com",
         password: "password",
