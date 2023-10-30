3938853e51cf77e62214bf579d449455f2fced53
sohamkasar19 <sohamkasar19@gmail.com>
2023-10-29 22:08:54 -0700

    add tablename to model

--- backend/sequelize/models/watchliststocks.js
+++ backend/sequelize/models/watchliststocks.js
@@ -1,14 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-  const WatchlistStock = sequelize.define("WatchlistStocks", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
+  const WatchlistStock = sequelize.define(
+    "WatchlistStocks",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      watchlist_id: DataTypes.UUID,
+      stock_id: DataTypes.UUID,
+      createdAt: DataTypes.DATE,
     },
-    watchlist_id: DataTypes.UUID,
-    stock_id: DataTypes.UUID,
-    createdAt: DataTypes.DATE,
-  });
+
+    {
+      tableName: "watchlist_stocks",
+    }
+  );
 
   return WatchlistStock;
 };
