13379a63abe0753db07a5e0c98ec613ecafc82b1
sohamkasar19 <sohamkasar19@gmail.com>
2023-10-30 17:13:58 -0700

    implement user apis

--- backend/routes/api/v1/users.js
+++ backend/routes/api/v1/users.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../../../controllers/UserController");
+const authenticateToken = require("../../../middlewares/authenticateToken");
 
 // Register user route
 router.post("/register", UserController.register);
+// Login route
+router.post("/login", UserController.login);
+// User profile route
+router.get("/me", authenticateToken, UserController.getUserProfile);
+// Update user profile route
+router.put("/me", authenticateToken, UserController.updateUserProfile);
 
 module.exports = router;
