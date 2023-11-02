f5a8d60f6dd64735e9df0c318d780ef193853261
Priyank Bardolia <bardoliapriyank@gmail.com>
2023-11-01 19:14:26 -0700

    Build stock search. Build AND OR functionality

--- backend/controllers/StockController.js
+++ backend/controllers/StockController.js
@@ -4,17 +4,19 @@ const { Stock } = require("../sequelize/models");
 exports.filterStocks = async (req, res) => {
   try {
     // Retrieve query parameters from the request
-    const { symbol, stockname, marketCap, price, priceEarning } = req.query;
+    const { symbol, stockname, marketCap, price, priceEarning, filterLogic } =
+      req.query;
 
     // Build a filter object based on the provided query parameters
     const filter = {};
 
     if (symbol) {
-      filter.symbol = symbol;
+      filter.symbol = { [Op.iLike]: `${symbol}%` };
     }
 
     if (stockname) {
-      filter.stockname = stockname;
+      // Use case-insensitive search for stockname
+      filter.stockname = { [Op.iLike]: `${stockname}%` };
     }
 
     if (marketCap) {
@@ -47,6 +49,16 @@ exports.filterStocks = async (req, res) => {
       }
     }
 
+    if (filterLogic === "OR") {
+      // Combine filter conditions using "OR" logic
+      const orConditions = Object.keys(filter).map((key) => ({
+        [key]: filter[key],
+      }));
+      filter[Op.or] = orConditions;
+      // Remove individual filter conditions
+      Object.keys(filter).forEach((key) => delete filter[key]);
+    }
+
     console.log(filter);
 
     const stocks = await Stock.findAll({ where: filter });
@@ -54,5 +66,26 @@ exports.filterStocks = async (req, res) => {
     res.status(200).json(stocks);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+exports.searchStocks = async (req, res) => {
+  try {
+    const { symbol, stockname } = req.query;
+
+    const filter = {};
+
+    if (symbol) {
+      filter.symbol = { [Op.iLike]: `${symbol}%` };
+    }
+    if (stockname) {
+      filter.stockname = { [Op.iLike]: `${stockname}%` };
+    }
+
+    const stocks = await Stock.findAll({ where: filter });
+
+    res.status(200).json(stocks);
+  } catch (error) {
+    res.status(500).json({ error: "Search Error" });
   }
 };
--- backend/routes/api/v1/stocks.js
+++ backend/routes/api/v1/stocks.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const StockController = require("../../../controllers/StockController");
 
 router.get("/filterStocks", StockController.filterStocks);
+router.get("/searchStocks", StockController.searchStocks);
 
 module.exports = router;
